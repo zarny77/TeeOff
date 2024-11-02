4e3480247ae40ce414343eafc71bf7e7fa14776c
zarny77 <zarndy77@gmail.com>
2024-11-01 19:21:26 -0500

    Initial Commit

---
+++ TeeOff/TeeOffApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeeOffApp.swift
+//  TeeOff
+//
+//  Created by Dylan Zarn on 2024-11-01.
+//
+
+import SwiftUI
+
+@main
+struct TeeOffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TeeOffTests/TeeOffTests.swift
@@ -0,0 +1,17 @@
+//
+//  TeeOffTests.swift
+//  TeeOffTests
+//
+//  Created by Dylan Zarn on 2024-11-01.
+//
+
+import Testing
+@testable import TeeOff
+
+struct TeeOffTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
