dc336e09cd1463a35ebdccdc3fdd7ba0266fc0bd
Dylan Zarn <zarndy77@gmail.com>
2024-12-07 21:19:43 -0600

    Adds the ViewModel for the CourseOverviewView Functionally identical to CourseCardViewModel at the moment, will be used later to add in personal stats in the course overview

---
+++ ViewModels/CourseOverviewViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  CourseDetailViewModel.swift
+//  TeeOff
+//
+//  Created by Dylan Zarn on 2024-12-07.
+//
+
+import Foundation
+import SwiftUI
+
+struct CourseOverviewViewModel {
+    let course: Course
+    
+    init(course: Course) {
+        self.course = course
+    }
+    
+    var name: String { course.id }
+    var address: String { course.address }
+    
+    var parDisplay: String { "Par \(course.par)"}
+    
+    var blues: Int { course.blues }
+    var whites: Int { course.whites }
+    var reds: Int { course.reds }
+    
+    var yardageData: [(yardage: Int, color: Color)] {
+        [
+            (course.blues, .blue),
+            (course.whites, .white),
+            (course.reds, .red)
+        ]
+    }
+    
+    var frontPar: Int { course.frontPar }
+    var backPar: Int { course.backPar }
+}
