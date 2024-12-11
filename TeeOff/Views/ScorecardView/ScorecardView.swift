888cee14961014ec28939bdb01485935f645dbaf
Dylan Zarn <zarndy77@gmail.com>
2024-12-11 17:49:36 -0600

    Refactors ScorecardView, adds logger Moves components into private var subviews Updated print functions to use logger instead

--- TeeOff/Views/ScorecardView/ScorecardView.swift
+++ TeeOff/Views/ScorecardView/ScorecardView.swift
@@ -10,8 +10,11 @@ import SwiftData
 
 struct ScorecardView: View {
         
-    // define column count and spacing
+    // MARK: - Properties
+    
     let columnLayout = Array(repeating: GridItem(.flexible(), spacing: 10), count: 2)
+    private let logger = Logger(origin: "ScorecardView")
+    
     @Bindable var round: RoundModel
     
     @Environment(\.modelContext) private var modelContext
@@ -19,58 +22,75 @@ struct ScorecardView: View {
     @State private var showingFinishConfirmation = false
     @State private var roundManager = RoundManager.shared
     
+    // MARK: - Body
     var body: some View {
         
-            // hole chip generator
-            ScrollView {
-                LazyVGrid(columns: columnLayout, spacing: 10, content:  {
-                    ForEach(round.course.holes) { hole in
-                        HoleScoringView(hole: hole, round: round)
-                    }
-                })
-                .padding(.horizontal, 10)
-                
-                HStack(spacing: 10) {
-                    Button {
-                        showingFinishConfirmation = true
-                        print("User Pressed Quit")
-                    } label: {
-                        Label("Finish Round", systemImage: "flag.checkered.circle.fill")
-                            .font(.title2)
-                            .fontWeight(.bold)
-                            .frame(maxWidth: .infinity)
-                            .frame(height: 60)
-                    }
-                    .buttonStyle(.bordered)
-                    .padding(.horizontal, 10)
-                    .foregroundStyle(.green)
-                }
-                .navigationTitle(round.course.id)
-                .navigationBarTitleDisplayMode(.automatic)
-                .confirmationDialog(
-                    "Finish Round",
-                    isPresented: $showingFinishConfirmation,
-                    titleVisibility: .automatic
-                ) {
-                    Button("Save", role: .none) {
-                        roundManager.activeRound = nil
-                        dismiss()
-                    }
-                    
-                    Button("Discard", role: .destructive) {
-                        modelContext.delete(round)
-                        roundManager.activeRound = nil
-                        dismiss()
-                        print("Round Successfully Deleted")
-                    }
-                } message: {
-                    Text("Save this round?")
-                        .font(.title)
+        // hole chip generator
+        ScrollView{
+            scorecardGrid
+            finishButton
+        }
+    }
+    
+    // MARK: - Components
+    
+    private var scorecardGrid: some View {
+        LazyVGrid(
+            columns: columnLayout, spacing: 10, content:  {
+                ForEach(round.course.holes) { hole in
+                    HoleScoringView(hole: hole, round: round)
                 }
             }
-            
+        )
+        .padding(.horizontal, 10)
+    }
+    
+     
+    private var finishButton: some View {
+        HStack(spacing: 10) {
+            Button {
+                showingFinishConfirmation = true
+                logger.log("'Finish Round' pressed", level: .info)
+            } label: {
+                Label("Finish Round", systemImage: "flag.checkered.circle.fill")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 60)
+            }
+            .buttonStyle(.bordered)
+            .padding(.horizontal, 10)
+            .foregroundStyle(.green)
         }
+        .navigationTitle(round.course.id)
+        .navigationBarTitleDisplayMode(.automatic)
+        .confirmationDialog(
+            "Finish Round",
+            isPresented: $showingFinishConfirmation,
+            titleVisibility: .automatic
+            
+        ) {
+            Button("Save", role: .none) {
+                roundManager.activeRound = nil
+                dismiss()
+                logger.log("Saved round", level: .success)
+            }
+            
+            Button("Discard", role: .destructive) {
+                modelContext.delete(round)
+                roundManager.activeRound = nil
+                dismiss()
+                logger.log("Round Deleted", level: .success)
+            }
+        } message: {
+            Text("Save this round?")
+                .font(.title)
+        }
+    }
+        
 }
+
+// MARK: - Preview
 
 #Preview {
     ScorecardView(round: PreviewData.goodRound)
