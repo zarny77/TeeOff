a4ec96b00a82ea711cae854e805569e047f378e6
Dylan Zarn <zarndy77@gmail.com>
2024-12-12 22:28:00 -0600

    Adds the StatManager class Contains all the computed properties from the list of past rounds.

---
+++ TeeOff/Views/ProfileView/StatSummaryView.swift
@@ -0,0 +1,18 @@
+//
+//  StatSummaryView.swift
+//  TeeOff
+//
+//  Created by Dylan Zarn on 2024-12-12.
+//
+
+import SwiftUI
+
+struct StatSummaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatSummaryView()
+}
